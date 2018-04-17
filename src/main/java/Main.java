5f8bc2b4143cc2c29650064290fe72dc5abf9d10
Piotr Cużytek <kenshin985@gmail.com>
2018-04-17 11:27:04 +0200

    changed title -> Deus Song; changed width and height of main window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,11 +9,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Deus Song");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
