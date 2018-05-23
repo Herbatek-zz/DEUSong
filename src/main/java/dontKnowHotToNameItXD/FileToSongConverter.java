1944c3bbe46c66ca4f43a71a8c9d9a3a7bd28638
Piotr Cużytek <kenshin985@gmail.com>
2018-05-23 09:51:59 +0200

    Update FileToSongConverter.java

--- src/main/java/dontKnowHotToNameItXD/FileToSongConverter.java
+++ src/main/java/dontKnowHotToNameItXD/FileToSongConverter.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class FileToSongConverter {
 
     public static Song toSong(File file, String category) {
-        return new Song(file.getName(), category, file.getPath().toString());
+        return new Song(file.getName(), category, file.getPath().toString()); // po co file.getPath().toString(), skoro .getPath zwraca stringa
     }
 
 }
